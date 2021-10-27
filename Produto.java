9f83d2b709a95cc4c2cc102dee6cac0f9d5b80e1
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2021-10-27 20:32:47 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,6 +1,6 @@
-public class Produto { //extends Produto
-	private int lote, codigo;
-	private String nome;
+public class Produto {
+	private int lote, codigo, estoque;
+	private String nome, categoria;
 	private float precoUnitario;
 	
 	public int getCodigo() {
@@ -9,26 +9,24 @@ public class Produto { //extends Produto
 	
 	public boolean setCodigo(int codigo) {
         if(codigo>0 && codigo<=99999) {
-            this.codigo = codigo;//definir
-            return true;
+            this.codigo = codigo;
+            return true; //seta o código
         }
         else
-            return false;
-        //mensagem de aviso, codigo invalido
+            return false; //mensagem de aviso, codigo invalido
     }
 	
 	public int getLote() {
 		return lote;
 	}
-	
+
 	public boolean setLote(int lote) {
 		if(lote > 100000 && lote <= 999999) {
 			this.lote = lote;
-		return true;
+		return true; //seta o lote
 		}
 		else
-			return false;
-			//mensagem de aviso, lote invalido
+			return false; //mensagem de aviso, lote invalido
 	}
 	
 	public float getPrecoUnitario() {
@@ -38,11 +36,10 @@ public class Produto { //extends Produto
 	public boolean setPrecoUnitario(float precoUnit) {
 		if(precoUnitario>=0) {
 			this.precoUnitario = precoUnit;
-			return true;
+			return true; //seta o preço unitário
 			}
 		else
-			return false;
-			//mensagem de aviso, preco invalido
+			return false; //mensagem de aviso, preco invalido
 	}
 
 	public String getNome() {
@@ -52,12 +49,35 @@ public class Produto { //extends Produto
 	public boolean setNome(String nome) {
 		if(nome.length()>2 && nome.length()<30) {
 			this.nome = nome;
-			return true;
+			return true; //seta o nome
 		}
 		else
-			return false;
-			//mensagem de aviso, nome invalido
+			return false; //mensagem de aviso, nome invalido
 	}
 	
-	
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public boolean setEstoque(int estoque) {
+		if(estoque >= 0) {
+			this.estoque = estoque;
+			return true; //seta o estoque
+			}
+		else
+			return false; //mensagem de aviso, nome invalido
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public boolean setCategoria(String categoria) {
+		if(categoria.length()>2 && nome.length()<30) {
+			this.categoria = categoria;
+		 	return true; //seta categoria
+		}
+		else
+			return false; //mensagem de aviso, nome invalido
+	}
 }
