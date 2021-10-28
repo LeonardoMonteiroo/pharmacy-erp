eb207842efbfc4a57926a65b5fec06ab4a6c9da7
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-10-27 21:14:30 -0300

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -1,6 +1,6 @@
-public class Produto {
-	private int lote, codigo, estoque;
-	private String nome, categoria;
+public class Produto { //extends Produto
+	private int codigo;
+	private String nome;
 	private float precoUnitario;
 	
 	public int getCodigo() {
@@ -9,26 +9,15 @@ public class Produto {
 	
 	public boolean setCodigo(int codigo) {
         if(codigo>0 && codigo<=99999) {
-            this.codigo = codigo;
-            return true; //seta o código
+            this.codigo = codigo;//definir
+            return true;
         }
         else
-            return false; //mensagem de aviso, codigo invalido
+            return false;
+        //mensagem de aviso, codigo invalido
     }
 	
-	public int getLote() {
-		return lote;
-	}
 
-	public boolean setLote(int lote) {
-		if(lote > 100000 && lote <= 999999) {
-			this.lote = lote;
-		return true; //seta o lote
-		}
-		else
-			return false; //mensagem de aviso, lote invalido
-	}
-	
 	public float getPrecoUnitario() {
 		return precoUnitario;
 	}
@@ -36,10 +25,11 @@ public class Produto {
 	public boolean setPrecoUnitario(float precoUnit) {
 		if(precoUnitario>=0) {
 			this.precoUnitario = precoUnit;
-			return true; //seta o preço unitário
+			return true;
 			}
 		else
-			return false; //mensagem de aviso, preco invalido
+			return false;
+			//mensagem de aviso, preco invalido
 	}
 
 	public String getNome() {
@@ -49,35 +39,19 @@ public class Produto {
 	public boolean setNome(String nome) {
 		if(nome.length()>2 && nome.length()<30) {
 			this.nome = nome;
-			return true; //seta o nome
+			return true;
 		}
 		else
-			return false; //mensagem de aviso, nome invalido
+			return false;
+			//mensagem de aviso, nome invalido
 	}
 	
-	public int getEstoque() {
-		return estoque;
-	}
+	
 
-	public boolean setEstoque(int estoque) {
-		if(estoque >= 0) {
-			this.estoque = estoque;
-			return true; //seta o estoque
-			}
-		else
-			return false; //mensagem de aviso, nome invalido
+	public void setVisible(boolean b) { //VER COM O RODRIGO PQ O ECLIPSE PEDIU PR CRIAR, ER EM TELAPRODUTOS SET VISIBLE
+		// TODO Auto-generated method stub
+		
 	}
-
-	public String getCategoria() {
-		return categoria;
-	}
-
-	public boolean setCategoria(String categoria) {
-		if(categoria.length()>2 && nome.length()<30) {
-			this.categoria = categoria;
-		 	return true; //seta categoria
-		}
-		else
-			return false; //mensagem de aviso, nome invalido
-	}
+	
+	
 }
