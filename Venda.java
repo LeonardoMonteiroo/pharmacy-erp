bf4256a8319892c935cbc64679ad07e2c71cdbb6
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2021-11-03 21:36:41 -0300

    Create Venda.java

---
+++ Venda.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Venda {
+	private ArrayList<Pedido> qntd = new ArrayList<Pedido>();
+	private ArrayList<Produto> produto = new ArrayList<Produto>();
+	private Pedido valorTotal;
+	private Pedido numCaixa;
+	private ArrayList<Integer> sequencia = new ArrayList<Integer>();
+	private int[] formaPagamento = {1, 2, 3, 4};//1-Dinheiro, 2-CC, 3-CD, 4-pix
+	private Date dataEmissao;
+	
+	/*public Pedido getQntd(int indice) {
+		return qntd.get(indice);
+	}
+	public Produto getProduto(int indice) {
+		return produto.get(indice);
+	}
+	public Pedido getValorTotal() {
+		return valorTotal;
+	}
+	public Pedido getNumCaixa() {
+		return numCaixa;
+	}*/
+	
+	public void setQntd(Pedido qntdItems) {
+		qntd.add(qntdItems);
+	}
+	
+	public void setProduto(Pedido produto) {
+		if(Integer.parseInt(produto.toString())>0)
+			qntd.add(produto);
+	}
+	
+	public void setValorTotal(Pedido valorTotal) {
+		if(Integer.parseInt(valorTotal.toString())>0)
+			this.valorTotal = valorTotal;
+		}
+	
+	public void setNumCaixa(Pedido numCaixa) {
+		if(Integer.parseInt(valorTotal.toString())>0)
+			this.numCaixa = numCaixa;
+	}
+	
+	public ArrayList<Integer> getSequencia() {
+		return sequencia;
+	}
+	
+	public void setSequencia(ArrayList<Integer> sequencia) {
+		if(Integer.parseInt(sequencia.toString())>0 && Integer.parseInt(sequencia.toString())<9999)
+			this.sequencia = sequencia;
+	}
+	//continuar a partir daqui !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	public int[] getFormaPagamento() {
+		return formaPagamento;
+	}
+	
+	public void setFormaPagamento(int[] formaPagamento) {
+		this.formaPagamento = formaPagamento;
+	}
+	
+	public Date getDataEmissao() {
+		return dataEmissao;
+	}
+	
+	public void setDataEmissao(Date dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+}
