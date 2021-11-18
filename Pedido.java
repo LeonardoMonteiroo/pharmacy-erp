a370c7f5c57085e7fbf7c6e0cc35a54606c01b42
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2021-11-17 22:35:52 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,49 +1,54 @@
-import java.util.ArrayList;
+package classesdenegocio;
 
 public class Pedido{
 	//atributos
-	
 	private int codPed=0;
-	private ArrayList<Integer> qntd = new ArrayList<Integer>();
-	private ArrayList<Produto> produto = new ArrayList<Produto>();
+	private int qntd;
+	private Produto produto;
 	private float valorTotal;
 	private Caixa numCaixa;
 	
 	public int getCodPed() {
 		return codPed;
 	}
+	
 	public void setCodPed(int codPed) {
 		this.codPed = codPed;
 	}
-	public int getQntd(int indice) {
-		return qntd.get(indice);
+	
+	public int getQntd() {
+		return qntd;
 	}
-	public void setQntd(int qntdItems) {
-		if(qntdItems>0)
-			qntd.add(qntdItems);
+	
+	public void setQntd(int qntd) {
+		if(qntd>0)
+			this.qntd= qntd;
 	}
-	public Produto getProduto(int indice) {
-		return produto.get(indice);
+	
+	public Produto getProduto() {
+		return produto;
 	}
+	
 	public void setProduto(Produto produto) {
 		if(Integer.parseInt(produto.toString())>0)
-			qntd.add(Integer.parseInt(produto.toString()));
+			this.produto = produto;
 	}
+	
 	public float getValorTotal() {
 		return valorTotal;
 	}
+	
 	public void setValorTotal(float valorItem, int[] qntd) {
 		for(int i=0; i<qntd.length; i++)
 		this.valorTotal+=qntd.length*valorItem;
 	}
+	
 	public Caixa getNumCaixa() {
 		return numCaixa;
 	}
+	
 	public void setNumCaixa(Caixa numCaixa) {
 		if(numCaixa.getAberto() == true)
 		this.numCaixa = numCaixa;
 	}
-
-	//array dos codigos pra garantir que n vao se repetir (fazer um if no setCod)
-
 }
