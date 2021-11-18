6fd91ceb01cb511985ccd3ebc941034a2a1f56ec
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-11-17 23:29:28 -0300

    Update Medicamento.java

--- Medicamento.java
+++ Medicamento.java
@@ -1,21 +1,27 @@
+package classesdenegocio;
+
+
 public class Medicamento extends Produto{
-	private int[] tipo = {1, 2};//1-comprimidos/capsulas, 2-ml
+	private String tipo; //comprimidos, capsulas, ml
 	private int qtdProduto;//qtd de comprimidos, ml's
 	private float concentracao;//quantos mg/comprimido, mg/ml
 	
-	public int[] getTipo() {
+	public String getTipo() {
 		return tipo;
 	}
-	public void setTipo(int indice) {//indice = tipo
-		if(indice>0 && indice<3)
-			this.tipo[indice] = indice;
+	public void setTipo(String tipo) {//indice = tipo
+		tipo.toUpperCase();		
+		if(tipo == "COMPRIMIDOS" || tipo == "COMPRIMIDO" || tipo == "CAPSULAS" || tipo == "CAPSULA" || tipo == "ML"){
+		this.tipo = tipo;
+		}
 	}
 	public int getQtdProduto() {
 		return qtdProduto;
 	}
 	public void setQtdProduto(int qtdProduto) {
-		if(qtdProduto>0)
+		if(qtdProduto>0) {
 			this.qtdProduto = qtdProduto;
+		}
 	}
 	public float getConcentracao() {
 		return concentracao;
