6ed11cbaba2957b69de6a9ef1240e695c8652452
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2021-11-17 22:35:13 -0300

    Update Venda.java

--- Venda.java
+++ Venda.java
@@ -1,35 +1,38 @@
-import java.util.ArrayList;
+package classesdenegocio;
 import java.util.Date;
 
 public class Venda {
-	private ArrayList<Pedido> qntd = new ArrayList<Pedido>();
-	private ArrayList<Produto> produto = new ArrayList<Produto>();
+	private int sequencia;
+	private Pedido qntd;
+	private Pedido produto;
 	private Pedido valorTotal;
 	private Pedido numCaixa;
-	private ArrayList<Integer> sequencia = new ArrayList<Integer>();
 	private int[] formaPagamento = {1, 2, 3, 4};//1-Dinheiro, 2-CC, 3-CD, 4-pix
 	private Date dataEmissao;
 	
-	/*public Pedido getQntd(int indice) {
-		return qntd.get(indice);
+	public int getQntd() {
+		return Integer.parseInt(qntd.toString());
 	}
-	public Produto getProduto(int indice) {
-		return produto.get(indice);
-	}
-	public Pedido getValorTotal() {
-		return valorTotal;
-	}
-	public Pedido getNumCaixa() {
-		return numCaixa;
-	}*/
 	
-	public void setQntd(Pedido qntdItems) {
-		qntd.add(qntdItems);
+	public int getProduto() {
+		return Integer.parseInt(produto.toString());
+	}
+	
+	public float getValorTotal() {
+		return Integer.parseInt(valorTotal.toString());
+	}
+	
+	public int getNumCaixa() {
+		return Integer.parseInt(numCaixa.toString());
+	}
+	
+	public void setQntd(Pedido qntd) {
+		this.qntd = qntd;
 	}
 	
 	public void setProduto(Pedido produto) {
 		if(Integer.parseInt(produto.toString())>0)
-			qntd.add(produto);
+			this.produto = produto;
 	}
 	
 	public void setValorTotal(Pedido valorTotal) {
@@ -42,14 +45,15 @@ public class Venda {
 			this.numCaixa = numCaixa;
 	}
 	
-	public ArrayList<Integer> getSequencia() {
+	public int getSequencia() {
 		return sequencia;
 	}
 	
-	public void setSequencia(ArrayList<Integer> sequencia) {
-		if(Integer.parseInt(sequencia.toString())>0 && Integer.parseInt(sequencia.toString())<=9999)//sequência de 1 até 9999
+	public void setSequencia(int sequencia) {
+		if(sequencia>0 && sequencia<=9999)//sequência de 1 até 9999
 			this.sequencia = sequencia;
 	}
+	
 	public int[] getFormaPagamento() {
 		return formaPagamento;
 	}
