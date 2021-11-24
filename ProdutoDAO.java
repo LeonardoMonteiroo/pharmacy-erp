46839595ea3e567fefb8df5d7e85ef56367187fd
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-11-24 20:35:20 -0300

    Update ProdutoDAO.java

--- ProdutoDAO.java
+++ ProdutoDAO.java
@@ -1,7 +1,9 @@
 package dados;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Connection;
 
 import classesdenegocio.Cosmeticos;
@@ -27,21 +29,81 @@ public class ProdutoDAO {
 		// ? = 3 -> marca
 		comando.setString(3, p.getMarca());
 		
-		comando.setString(4, m.getTipo());
+		comando.setInt(4, c.getEstoque());
 		
-		comando.setInt(5, m. getQtdProduto());
+		comando.setString(5, c.getEspecificao());
 		
-		comando.setFloat(6, m.getConcentracao());
-		//TA RECEBENDO NA ORDEM ERRADA (ARRUMAR)
-		comando.setString(7, c. getEspecificao());
+		comando.setString(6, m.getTipo());
+		comando.setString(6, c.getTipo());
 		
-		comando.setString(8, c.getTipo());
+		comando.setString(7, c.getUnidMedidaProd());
 		
-		comando.setString(9, c.getUnidMedidaProd());
+		comando.setInt(8, m.getQtdProduto());
+		
+		comando.setFloat(9, m.getConcentracao());		
+		
 		//executar o comando SQL
 		comando.execute();
 		
 		//fechar a conexão!
 		con.close();
 	}
+	
+	public void alterar(Produto p, Cosmeticos c, Medicamento m) throws ClassNotFoundException, SQLException {
+		//Comando SQL -> insert...
+		String sql = "UPDATE produto SET nome = ?, precoUnitario = ?, marca = ?, estoque = ?, especificacao = ?, tipo = ?, unidMedidaProd = ?, qtdProduto = ?, concentracao = ? WHERE codigo = ?;";		
+					//Conectar ao BD
+		Farmacia conexao1 = new Farmacia();
+		
+		//Connection = static
+		Connection con = conexao1.conectar();
+		//definir dados que serão gravados na(s) tabela(s)
+		PreparedStatement comando = con.prepareStatement(sql);
+		// ? = 1 -> nome
+		comando.setString(1, p.getNome());
+		// ? = 2 -> preco
+		comando.setFloat(2, p.getPrecoUnitario());
+		// ? = 3 -> marca
+		comando.setString(3, p.getMarca());
+		
+		comando.setInt(4, c.getEstoque());
+		
+		comando.setString(5, c.getEspecificao());
+		
+		comando.setString(6, m.getTipo());
+		
+		comando.setString(7, c.getUnidMedidaProd());
+		
+		comando.setInt(8, m.getQtdProduto());
+		
+		comando.setFloat(9, m.getConcentracao());	
+		
+		comando.setInt(10, p.getCodigo());
+		
+		//executar o comando SQL
+		comando.execute();
+		
+		//fechar a conexão!
+		con.close();
+	}
+
+	public void deletar(Produto p) throws ClassNotFoundException, SQLException {
+		
+		String sql = "delete from produto where codigo = ?;";		
+					//Conectar ao BD
+		Farmacia conexao2 = new Farmacia();
+		
+		//Connection = static
+		Connection con = conexao2.conectar();
+		//definir dados que serão gravados na(s) tabela(s)
+		PreparedStatement comando = con.prepareStatement(sql);
+		// ? = 1 -> nome
+		comando.setInt(1, p.getCodigo());
+		//executar o comando SQL
+		comando.execute();
+		
+		//fechar a conexão!
+		con.close();
+	}
+	
 }
