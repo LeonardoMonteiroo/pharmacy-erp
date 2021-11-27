3fd2dad082def929fc92134145251dcd1b954d11
LeonardoMonteiroo <92556142+LeonardoMonteiroo@users.noreply.github.com>
2021-11-26 23:37:08 -0300

    Update CaixaDAO.java

--- CaixaDAO.java
+++ CaixaDAO.java
@@ -1,7 +1,9 @@
 package dados;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Connection;
 
 import classesdenegocio.Caixa;
@@ -48,8 +50,8 @@ public class CaixaDAO {
 	public void saldoCaixa(Caixa c) throws ClassNotFoundException, SQLException { //p=novos valores do produto
 		//Comando SQL -> insert...
 		String sql = "update caixa set "
-					+ "saldo='?' "
-					+ "where numCaixa like '?'"; 
+					+ "saldoCaixa=? "
+					+ "where numCaixa like ?"; 
 		//Criar o objeto para conexão com BD
 		Farmacia conexao = new Farmacia(); //-> 
 		//Conectando ao BD
@@ -73,12 +75,45 @@ public class CaixaDAO {
 		Farmacia conexao = new Farmacia(); //-> 
 		//Conectando ao BD
 		Connection con = conexao.conectar(); //-> .conectar()
-				//Criar um objeto que constroi o comando SQL
+		//Criar um objeto que constroi o comando SQL
 		PreparedStatement comando = con.prepareStatement(sql);
 				// 1 -> ?
 		comando.setInt(1, c.getNumCaixa());
 		comando.execute();
 				//fechar a conexão!
 		con.close();		
+	}
+	
+	public ArrayList<Caixa> listar(Caixa c) throws SQLException, ClassNotFoundException{ //retornar os dados SELECT
+		//Comando SQL -> insert...
+		String sql = "select * "
+					+ "from caixa "
+					+ "where numCaixa like ?";
+					 
+		//Criar o objeto para conexão com BD
+		Farmacia conexao = new Farmacia(); //-> 
+		//Conectando ao BD
+		Connection con = conexao.conectar(); //-> .conectar()
+		//Criar um objeto que constroi o comando SQL
+		PreparedStatement comando = con.prepareStatement(sql);
+		
+		// 1 -> ? parâmetros where nome like ? or preco >= ...
+		comando.setInt(1, c.getNumCaixa());
+		
+		ArrayList<Caixa> resultado = new ArrayList<Caixa>();
+		//ResultSet - conjunto de dados do SELECT
+		ResultSet rs = comando.executeQuery(); 
+			
+		//.next() -> próximo resultado do ResultSet
+		while(rs.next()) { //repetir para o número de linhas do BD
+			Caixa c1 = new Caixa();
+			c1.setNumCaixa(rs.getInt("numCaixa"));
+			c1.setAberto(rs.getBoolean("aberto"));
+			c1.setSaldoCaixa(rs.getFloat("saldoCaixa"));
+			resultado.add(c1);
+		} //repete enquanto o .next() == true
+			
+		con.close();
+		return resultado;
 	}
 }
