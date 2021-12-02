329829ae2c45cb07ab99d9fdcec9b39912d825be
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-12-01 21:12:18 -0300

    Update TelaEntrada.java

--- TelaEntrada.java
+++ TelaEntrada.java
@@ -1,3 +1,4 @@
+package tela;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -5,6 +6,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+
+import classesdenegocio.Pedido;
+import classesdenegocio.Produto;
+import dados.ProdutoDAO;
+
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -13,6 +19,7 @@ import javax.swing.JList;
 import javax.swing.JScrollBar;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class TelaEntrada extends JFrame {
@@ -29,7 +36,6 @@ public class TelaEntrada extends JFrame {
 	private DefaultTableModel obj1 = new DefaultTableModel(colunas, 0);
 
 	int check = 0;
-	private JTable table;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -50,82 +56,29 @@ public class TelaEntrada extends JFrame {
 	public TelaEntrada() {
 		setTitle("Entrada");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 350, 136);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel_2 = new JLabel("C\u00F3digo");
-		lblNewLabel_2.setBounds(10, 11, 86, 14);
+		lblNewLabel_2.setBounds(10, 49, 86, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		txCod = new JTextField();
-		txCod.setBounds(10, 24, 75, 20);
+		txCod.setBounds(10, 62, 75, 20);
 		contentPane.add(txCod);
 		txCod.setColumns(10);
 		
 		JLabel lblNewLabel_3_1 = new JLabel("Quantidade");
-		lblNewLabel_3_1.setBounds(106, 10, 75, 14);
+		lblNewLabel_3_1.setBounds(106, 48, 75, 14);
 		contentPane.add(lblNewLabel_3_1);
 		
 		txQuantidade = new JTextField();
 		txQuantidade.setColumns(10);
-		txQuantidade.setBounds(106, 24, 75, 20);
+		txQuantidade.setBounds(106, 62, 75, 20);
 		contentPane.add(txQuantidade);
-		
-		JButton btnNewButton = new JButton("Adicionar");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				Produto prod = new Produto();
-				prod.setCodigo(Integer.parseInt(txCod.getText()));
-				
-				Pedido p1 = new Pedido();
-				p1.setQntd(Integer.parseInt(txQuantidade.getText())); 
-							
-				//nao mexer, gambiarra!!!
-				if(check==0) {
-				obj1.addRow(new String[] {
-						colunas[0],
-						colunas[1],
-						colunas[2]
-				});
-				check=1;
-				}
-				
-				if(check==1 || check==2){ //testar se funciona
-				
-				obj1.addRow(new String[] {
-						String.valueOf(prod.getNome()), 
-						String.valueOf(prod.getCodigo()),
-						String.valueOf(p1.getQntd()),
-						String.valueOf(prod.getPrecoUnitario()) 
-				});
-				
-				check=2;
-				
-				}
-				/*else {
-					
-					obj1.addRow(new String[] {
-							p1.getNome(), 
-							String.valueOf(p1.getCodigo()),
-							String.valueOf(p1.getQnt()),
-							String.valueOf(p1.getPrecoUnitario())
-					});
-				}*/
-				
-				table.setModel(obj1);
-				
-			}
-		});
-		btnNewButton.setBounds(236, 23, 89, 23);
-		contentPane.add(btnNewButton);
-		
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(308, 55, 17, 195);
-		contentPane.add(scrollBar);
 		
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.addActionListener(new ActionListener() {
@@ -135,16 +88,26 @@ public class TelaEntrada extends JFrame {
 				dispose();
 			}
 		});
-		btnVoltar.setBounds(325, 227, 99, 23);
+		btnVoltar.setBounds(10, 15, 99, 23);
 		contentPane.add(btnVoltar);
 		
-		table = new JTable();
-		table.setBorder(UIManager.getBorder("FormattedTextField.border"));
-		table.setBounds(10, 55, 314, 195);
-		contentPane.add(table);
-		
 		JButton btnDarEntrada = new JButton("Dar entrada");
-		btnDarEntrada.setBounds(325, 193, 99, 23);
+		btnDarEntrada.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Produto p = new Produto();
+				p.setCodigo(Integer.parseInt(txCod.getText()));
+				p.setEstoque(Integer.parseInt(txQuantidade.getText()));
+				
+				ProdutoDAO pDAO = new ProdutoDAO();
+				try {
+					pDAO.alterarEstoque(p);
+				} catch (ClassNotFoundException | SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} 
+			}
+		});
+		btnDarEntrada.setBounds(202, 61, 122, 23);
 		contentPane.add(btnDarEntrada);
 	}
 }
