31723745aabf177dbb8c11157556e5c0d5f19fd5
Natália Pisetta Fiorentin <82157666+natalia-pf@users.noreply.github.com>
2021-12-02 22:02:32 -0300

    Update TelaEstoque.java

--- TelaEstoque.java
+++ TelaEstoque.java
@@ -4,17 +4,24 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
+import classesdenegocio.Cosmeticos;
+import classesdenegocio.Medicamento;
 import classesdenegocio.Produto;
+import dados.ProdutoDAO;
 
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.UIManager;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import javax.swing.JTable;
 
 public class TelaEstoque extends JFrame {
 
@@ -45,19 +52,14 @@ public class TelaEstoque extends JFrame {
 	public TelaEstoque() {
 		setTitle("Estoque");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 401, 396);
+		setBounds(100, 100, 401, 186);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JList list = new JList();
-		list.setBorder(UIManager.getBorder("FileChooser.listViewBorder"));
-		list.setBounds(10, 65, 365, 266);
-		contentPane.add(list);
-		
 		JLabel lblNewLabel = new JLabel("C\u00F3digo:");
-		lblNewLabel.setBounds(10, 40, 46, 14);
+		lblNewLabel.setBounds(10, 25, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		campoCodigoDigitado = new JTextField();
@@ -66,17 +68,17 @@ public class TelaEstoque extends JFrame {
 		//		pesquisar.add(Integer.parseInt(campoCodigoDigitado.getText()));
 		//	}
 		//});
-		campoCodigoDigitado.setBounds(53, 37, 86, 20);
+		campoCodigoDigitado.setBounds(53, 22, 46, 20);
 		contentPane.add(campoCodigoDigitado);
 		campoCodigoDigitado.setColumns(10);
 		
 		JLabel lblNom = new JLabel("Nome:");
-		lblNom.setBounds(149, 40, 46, 14);
+		lblNom.setBounds(122, 25, 46, 14);
 		contentPane.add(lblNom);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(183, 37, 86, 20);
+		textField_1.setBounds(161, 22, 46, 20);
 		contentPane.add(textField_1);
 		
 		JButton btnNewButton = new JButton("Pesquisar");
@@ -89,7 +91,7 @@ public class TelaEstoque extends JFrame {
 				//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NAO FUNCIONA
 			}
 		});
-		btnNewButton.setBounds(286, 36, 89, 23);
+		btnNewButton.setBounds(233, 21, 131, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Voltar");
@@ -100,7 +102,7 @@ public class TelaEstoque extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton_1.setBounds(286, 6, 89, 23);
+		btnNewButton_1.setBounds(134, 113, 121, 23);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_1_1 = new JButton("Cadastrar produto");
@@ -111,7 +113,7 @@ public class TelaEstoque extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton_1_1.setBounds(10, 6, 121, 23);
+		btnNewButton_1_1.setBounds(10, 76, 172, 23);
 		contentPane.add(btnNewButton_1_1);
 		//abrir TelaCadastro
 		
@@ -123,8 +125,9 @@ public class TelaEstoque extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton_1_1_1.setBounds(148, 6, 121, 23);
+		btnNewButton_1_1_1.setBounds(203, 76, 172, 23);
 		contentPane.add(btnNewButton_1_1_1);
-		//abrir TelaLote
+		
+
 	}
 }
